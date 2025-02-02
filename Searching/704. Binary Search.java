2ef00943ff5682bd1b9719efabfcfdca33fd5951
Rishi-Thakre <134166337+Rishi-Thakre@users.noreply.github.com>
2025-02-02 23:04:12 +0530

    Create 704. Binary Search.java

---
+++ Searching/704. Binary Search.java
@@ -0,0 +1,22 @@
+// 704. Binary Search
+
+// https://leetcode.com/problems/binary-search/description/
+
+class Solution {
+    public int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end){
+            int mid = start +(end-start)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid] < target){
+                start = mid+1;
+            }
+            else end = mid-1;
+        }
+        return -1;
+    }
+}
