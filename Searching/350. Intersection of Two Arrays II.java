05e3888c1d4c8ffa38ed153690ead1726fcd210f
Rishi-Thakre <134166337+Rishi-Thakre@users.noreply.github.com>
2025-02-02 23:00:47 +0530

    Create 350. Intersection of Two Arrays II.java

---
+++ Searching/350. Intersection of Two Arrays II.java
@@ -0,0 +1,38 @@
+// 350. Intersection of Two Arrays II
+
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        List<Integer> list = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                
+                // if (list.isEmpty() || list.get(list.size() - 1) != nums1[i]) {
+                    list.add(nums1[i]);
+               // }
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        int[] arr = new int[list.size()];
+        for (int k = 0; k < list.size(); k++) {
+            arr[k] = list.get(k);
+        }
+
+        return arr;
+    }
+}
