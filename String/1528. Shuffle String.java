6b2f17b5b3391a3bd2f34edee29ccac47fea6eb1
Rishi-Thakre <134166337+Rishi-Thakre@users.noreply.github.com>
2025-04-23 18:06:44 +0530

    Create 1528. Shuffle String.java

---
+++ String/1528. Shuffle String.java
@@ -0,0 +1,15 @@
+// 1528. Shuffle String
+
+// https://leetcode.com/problems/shuffle-string/description/
+
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] shuffled = new char[s.length()];
+
+        for(int i = 0; i < indices.length; i++){
+            shuffled[indices[i]] = s.charAt(i);
+
+        }
+        return new String(shuffled);
+    }
+}
